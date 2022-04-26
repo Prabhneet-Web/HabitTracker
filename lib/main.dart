e839ad0d69530d812a35dd592cee5f51f1fc286a
Prabhneet Narula <imailcode@gmail.com>
2022-04-26 22:19:17 +0530

    Added the responsiveLayouts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/responsive/mobileScreenLayout.dart';
+import 'package:habit_tracker/responsive/responsiveLayoutScreen.dart';
+import 'package:habit_tracker/responsive/webScreenLayout.dart';
+import 'package:habit_tracker/util/colors.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,7 +19,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: const ResponsiveLayout(
+        webScreenLayout: WebScreenLayout(),
+        mobileScreenLayout: MobileScreenLayout(),
+      ),
     );
   }
 }
