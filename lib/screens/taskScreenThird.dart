3eb7a74b2ed746c55a97c3b75e6152185712a8b2
Prabhneet Narula <imailcode@gmail.com>
2022-05-01 00:27:56 +0530

    NewScreens Added.

--- lib/screens/taskScreenThird.dart
+++ lib/screens/taskScreenThird.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/util/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final appBar = AppBar(
+    title: const Text(
+      "Most Popular Habits",
+      style: TextStyle(fontSize: 22),
+    ),
+    backgroundColor: mobileBackgroundColor,
+    elevation: 0.0,
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 10.0),
+        child: GestureDetector(
+          onTap: () {},
+          child: Stack(
+            alignment: Alignment.center,
+            children: const [
+              Icon(
+                Icons.circle,
+                color: iconColor,
+                size: 37,
+              ),
+              Icon(
+                Icons.add,
+                color: primaryColor,
+                size: 23,
+              )
+            ],
+          ),
+        ),
+      )
+    ],
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final mediaQuery = MediaQuery.of(context);
+
+    return Scaffold(
+      appBar: appBar,
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 24),
+        child: Column(
+          children: [
+            //TaskUpperCard
+            Container(
+              height: (mediaQuery.size.height -
+                      appBar.preferredSize.height -
+                      mediaQuery.padding.top) *
+                  0.19,
+              color: Colors.grey,
+            ),
+
+            //TaskMiddleCard
+            
+
+            //TaskLowerCard
+          ],
+        ),
+      )),
+    );
   }
 }
